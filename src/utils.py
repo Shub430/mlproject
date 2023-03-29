72050d19be695e9016098db015725f8bbda65564
Shub430 <khantwal430@gmail.com>
2023-03-30 01:58:50 +0530

    data_transformation

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,10 @@ import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 import dill
+
+
 
 def save_object(file_path,obj):
     try:
@@ -14,5 +17,21 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(x_train,y_train)
+
+            y_train_prediction=model.predict(x_train)
+            y_test_prediction=model.predict(x_test)
+            
+            train_model_score=r2_score(y_train,y_train_prediction)
+            test_model_score=r2_score(y_test,y_test_prediction)
+        return report    
     except Exception as e:
         raise CustomException(e,sys)
